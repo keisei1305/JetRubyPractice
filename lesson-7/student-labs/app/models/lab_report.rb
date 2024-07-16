900b5435e68b5769427ef62ea85de6124932993c
keisei1305 <kippop88@gmail.com>
2024-07-16 16:48:27 +0300

    add validates for user and labs. And restore div for user to table

--- lesson-7/student-labs/app/models/lab_report.rb
+++ lesson-7/student-labs/app/models/lab_report.rb
@@ -1,3 +1,13 @@
 class LabReport < ApplicationRecord
   belongs_to :user
+
+  validates :title,
+  length: {maximum: 250}
+
+  validates :description,
+  length: {maximum: 500}
+
+  validates :grade,
+  format: { with: /\A(A|B|C|D|E|FX|F)\z/, 
+  message: "grade is incorrect"}
 end
--- lesson-7/student-labs/app/models/user.rb
+++ lesson-7/student-labs/app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  has_many :lab_report
+
+  validates :email, 
+  presence: true, 
+  uniqueness: true,
+  format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, 
+  message: "email is incorrect"},
+  length: {maximum: 150}
+
+  validates :first_name,
+  presence: true,
+  length: {maximum: 100}
+  
+  validates :last_name,
+  presence: true,
+  length: {maximum: 100}
 end
